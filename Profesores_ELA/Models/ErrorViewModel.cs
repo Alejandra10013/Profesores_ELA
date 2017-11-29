843cb31dd5110a4e9c07afc7381b33013e09221d
Alejandra10013 <kalejac@gmail.com>
2017-11-29 19:20:19 -0300

    Agregar archivos de proyecto.

---
+++ Profesores_ELA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Profesores_ELA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
